092230bc5c6cd07553581179309c0bb0c630a0c0
blougouras <luizblougouras5@gmail.com>
2025-03-21 19:31:47 -0300

    finalizando por enquanto a atualização do meu projeto de sistema bancário

--- projeto.py
+++ projeto.py
@@ -1,30 +1,38 @@
+import textwrap
+
 def menu():
 
-    print('''
+    print(textwrap.dedent('''
     =====SISTEMA BANCÁRIO=====
           D = Deposito 
           S = Saque 
           E = Extrato
-    ''')
+          NU = Criar Usuário
+          NC = Criar Conta
+          LC = Listar Contas
+          Q = Sair
+    '''))
 
-
-def depositoBancario(saldo, extrato):
+def depositoBancario(saldo, extrato, /):
     valorDeposito = float(input("insira um valor: "))
 
     if valorDeposito > 0:
         saldo += valorDeposito
+        print()
+        print("Depósito realizado com sucesso!")
         extrato += f"Depósito: R$ {valorDeposito:.2f}\n"
     else:
         print("Valor muito baixo ou negativo, não foi possível realizar a operação. Tente novamente!")
     
     return saldo, extrato
 
-def saqueBancario(saldo, extrato, limite, limiteSaques, numeroSaques):
+def saqueBancario(*, saldo, extrato, limite, limiteSaques, numeroSaques):
     if numeroSaques >= limiteSaques:
         print("Limite diário de saques atingido! Tente novamente amanhã.")
         return saldo, extrato, numeroSaques
     
     valorSaque = float(input("Insira um valor para saque: "))
+    print()
 
     if valorSaque <= 0:
         print("O valor do saque deve ser positivo.")
@@ -40,7 +48,7 @@ def saqueBancario(saldo, extrato, limite, limiteSaques, numeroSaques):
 
     return saldo, extrato, numeroSaques
 
-def exibirExtrato(extrato, saldo):
+def exibirExtrato(saldo, /, *, extrato):
         print("\n===================== EXTRATO =====================")
 
         if not extrato:
@@ -51,22 +59,89 @@ def exibirExtrato(extrato, saldo):
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("=====================================================")
 
-saldo = 0
-limite = 500
-extrato = ""
-numeroSaques = 0
-limiteSaques = 3
 
-while True:
-    menu()
+def criarUsuario(usuarios):
+    cpf = input("Informe o seu CPF (somente números): ")
+    usuario = filtrarUsuario(cpf, usuarios)
 
-    opc = input("O que deseja fazer: ")
+    if usuario:
+        print("\n Já existe um usuário com esse CPF!!")
+        return
+    
+    nome = input("Seu nome completo: ")
+    dataNascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe seu endereço (logradouro, nro - bairro - cidade / UF): ")
 
-    if opc == "d":
-        saldo, extrato = depositoBancario(saldo, extrato)
-    elif opc == "s":
-        saldo, extrato, numeroSaques = saqueBancario(saldo, extrato, limite, limiteSaques, numeroSaques)  
-    elif opc == "e":
-        exibirExtrato(extrato, saldo)
-    else:
-        break
+    usuarios.append({"nome": nome, "dataNascimento": dataNascimento, "cpf": cpf, "endereço": endereco})
+
+def filtrarUsuario(cpf, usuarios):
+    usuariosFiltrados = []
+    for usuario in usuarios:
+        if usuario["cpf"] == cpf:
+            usuariosFiltrados.append(usuario)
+    
+    return usuariosFiltrados[0] if usuariosFiltrados else None
+
+
+def criarConta(agencia, numeroConta, usuarios):
+    cpf = input("Informe o seu CPF (somente números): ")
+    usuario = filtrarUsuario(cpf, usuarios)
+
+    if usuario:
+        print("\n Conta criada com sucesso!")
+        return {"agencia": agencia, "numeroConta": numeroConta, "usuario": usuario}
+    
+    print("\n Usuário não encontrado, encerrando fluxo de criação de conta!")
+
+def listarContas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta["agencia"]}
+            C/C:\t{conta["numeroConta"]}
+            Titular:\t{conta["usuario"]["nome"]}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
+def main():
+
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numeroSaques = 0
+    limiteSaques = 3
+
+    agencia = "0001"
+    usuarios = []
+    contas = []
+
+
+    while True:
+        menu()
+
+        opc = input("O que deseja fazer: ")
+
+        if opc == "d":
+            saldo, extrato = depositoBancario(saldo, extrato)
+        elif opc == "s":
+            saldo, extrato, numeroSaques = saqueBancario(saldo = saldo, extrato = extrato, limite = limite, limiteSaques = limiteSaques, numeroSaques = numeroSaques)  
+        elif opc == "e":
+            exibirExtrato(saldo, extrato = extrato)
+        elif opc == "nu":
+            criarUsuario(usuarios)
+        elif opc == "nc":
+            numeroConta = len(contas) + 1
+            conta = criarConta(agencia, numeroConta, usuarios)
+
+            if conta:
+                contas.append(conta)
+        elif opc == "lc":
+            listarContas(contas)
+        elif opc == "q":
+            print("Agradeçemos a preferência, tenha um bom dia!!")
+            break
+        else:
+            print("Operação Inválida, tente novamente!")
+
+if __name__ == "__main__":
+    main()
